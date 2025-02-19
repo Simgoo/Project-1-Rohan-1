68ac32dfdb8f669b4ca94ffb8cf34eb210084962
Simgoo <vineethpateel@gmail.com>
2025-02-18 20:41:58 -0500

    changing build

--- build.sh
+++ build.sh
@@ -3,3 +3,6 @@ cd client
 npm install
 npm install --save-dev @babel/core @babel/preset-env @babel/preset-react babel-loader
 npm run build
+cd ..
+python3 manage.py makemigrations
+python3 manage.py migrate
