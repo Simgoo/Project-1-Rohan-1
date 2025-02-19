646fbff169f95aea801b5512807829a95e6b16aa
Simgoo <vineethpateel@gmail.com>
2025-02-18 20:51:32 -0500

    fixing more stuff

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 pip3 install -r requirements.txt
+python3 manage.py makemigrations
+python3 manage.py migrate
 cd client
 npm install
 npm install --save-dev @babel/core @babel/preset-env @babel/preset-react babel-loader
 npm run build
 cd ..
-python3 manage.py makemigrations
-python3 manage.py migrate
